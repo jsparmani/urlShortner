7685ea0be42e3964bc058a70e80f3e1afbab7d82
Shreya Gupta <shreyagupta3011@gmail.com>
2020-06-03 17:46:02 +0530

    Updates

---
+++ precache-manifest.032ce2d7ce82aa744eb24b8535488f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b1ed622566dd55ee98e994a46f27f5",
+    "url": "/urlShortner/index.html"
+  },
+  {
+    "revision": "87aebc03f5d91ad6ea66",
+    "url": "/urlShortner/static/css/main.075e99ec.chunk.css"
+  },
+  {
+    "revision": "282f25a936dd03098aee",
+    "url": "/urlShortner/static/js/2.2743bae1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/urlShortner/static/js/2.2743bae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87aebc03f5d91ad6ea66",
+    "url": "/urlShortner/static/js/main.b004cb6f.chunk.js"
+  },
+  {
+    "revision": "a3dd4bcac69b3120ca02",
+    "url": "/urlShortner/static/js/runtime-main.9be48907.js"
+  }
+]);
